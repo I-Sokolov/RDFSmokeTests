aeee7818333a3b7d5ba98ed0bc93fe9acfa4b03f
Igor.Sokolov <I.Sokolov@ya.ru>
2022-10-18 22:35:55 +0400

    initial templates

---
+++ CppEngineTests/Main.cpp
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+extern int main()
+{
+    printf("hello\n");
+    return 0;
+}
---
+++ CsIfcEngineTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsIfcEngineTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
