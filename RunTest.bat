e9128e6bb8e8bcec40aa36dca9f25eac8e727ace
Igor.Sokolov <I.Sokolov@ya.ru>
2022-10-27 11:27:44 +0400

    improve console output

--- RunTest.bat
+++ RunTest.bat
@@ -2,12 +2,14 @@
 
 echo Build %1 .......
 %RDF_TEST_DEVENV% %1\%1.sln /Build %2
-if not .%ERRORLEVEL% == .0 (echo !!!! Failed to build %1.vcxproj !!!! & goto END)
+@if not .%ERRORLEVEL% == .0 (echo !!!! Failed to build %1.vcxproj !!!! & goto END)
 
 echo Run %1 .........
 cd output
 %3\%1.exe %4 %5 %6 %7 %8 %9
-if not .%ERRORLEVEL% == .0 (echo !!!! Failed test %1.exe !!!! & goto END)
+@if not .%ERRORLEVEL% == .0 (echo !!!! Failed test %1.exe !!!! & goto END)
 cd ..
+
+@echo off
 
 :END
