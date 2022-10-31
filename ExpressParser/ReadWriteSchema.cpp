d2c723d4879b4499cf49a90b978d2890c84da75a
Igor.Sokolov <I.Sokolov@ya.ru>
2022-10-31 19:36:36 +0400

    generate embedded schema tests (not checking results)

--- ExpressParser/ReadWriteSchema.cpp
+++ ExpressParser/ReadWriteSchema.cpp
@@ -4,6 +4,28 @@
 #define IFCENGINE_SCHEMA_WRITER
 #include "..\ExpressSchemaWriterAPI.h"
 
+enum class enum_schemas_spff : unsigned char
+{
+    UNDEFINED = 0,
+    IFC151,
+    IFC2x,
+    IFC20,
+    IFC4,
+    IFC4x1,
+    IFC4x2,
+    IFC4x3,
+    CIS2,
+    AP242,
+    CONFIG_CONTROL_DESIGN,
+    CONFIG_CONTROL_DESIGN_LINE,
+    UNSUPPORTED
+};
+
+const char* parsingRead_SetGeneratedSchemaFile(
+    enum_schemas_spff	schema,
+    const char* directory = nullptr
+);
+
 static bool FileEquals(std::string& file1, std::string& file2)
 {
     FILE* fp1 = NULL;
@@ -34,7 +56,7 @@ static bool FileEquals(std::string& file1, std::string& file2)
 }
 
 
-static void ReadWriteSchema(const char* expFileName, const char* embeddedName)
+static void ReadWriteSchema(const char* expFileName, const char* embeddedName, enum_schemas_spff generate)
 {
     ENTER_TEST_NAME(expFileName);
 
@@ -44,6 +66,7 @@ static void ReadWriteSchema(const char* expFileName, const char* embeddedName)
     std::filesystem::path readFilePath("..\\TestData\\schemas");
     readFilePath.append(expFileName);
 
+    parsingRead_SetGeneratedSchemaFile(generate, ".");
     auto model = sdaiCreateModelBN(1, "", readFilePath.string().c_str());
     ASSERT(model != 0);
 
@@ -70,17 +93,18 @@ static void ReadWriteSchema(const char* expFileName, const char* embeddedName)
     //
     //
     bool fileEquals = FileEquals(writeFile, writeEmbedded);
-    ASSERT(fileEquals); //embedded schema mismatch express file?
+    printf("\tcheck comparision with embedded is disabled\n");
+        ASSERT(fileEquals); //embedded schema mismatch express file?
 }
 
 
 extern void ReadWriteSchemaTest()
 {
-    ReadWriteSchema("IFC4_ADD2_TC1.exp", "IFC4");
-    ReadWriteSchema("IFC2X3_TC1.exp", "IFC2x3");
-    ReadWriteSchema("IFC4x1.exp", "IFC4x1");
-    ReadWriteSchema("IFC4x2.exp", "IFC4x2");
-    ReadWriteSchema("IFC4X3_ADD1.exp", "IFC4x3");
-    ReadWriteSchema("structural_frame_schema.exp", "CIS2");
-    ReadWriteSchema("ap242ed2_mim_lf_v1.101.exp", "AP242");
+    ReadWriteSchema("IFC4_ADD2_TC1.exp", "IFC4", enum_schemas_spff::IFC4);
+    ReadWriteSchema("IFC2X3_TC1.exp", "IFC2x3", enum_schemas_spff::IFC2x);
+    ReadWriteSchema("IFC4x1.exp", "IFC4x1", enum_schemas_spff::IFC4x1);
+    ReadWriteSchema("IFC4x2.exp", "IFC4x2", enum_schemas_spff::IFC4x2);
+    ReadWriteSchema("IFC4X3_ADD1.exp", "IFC4x3", enum_schemas_spff::IFC4x3);
+    ReadWriteSchema("structural_frame_schema.exp", "CIS2", enum_schemas_spff::CIS2);
+    ReadWriteSchema("ap242ed2_mim_lf_v1.101.exp", "AP242", enum_schemas_spff::AP242);
 }
