457d01d596a4d3e3bd7f7b0c1baad1ac0abd0970
Igor.Sokolov <I.Sokolov@ya.ru>
2023-05-06 19:40:54 +0400

    Set/GetAppData, GetModel

--- CppEngineTests/ExtAppMem.cpp
+++ CppEngineTests/ExtAppMem.cpp
@@ -1,12 +1,40 @@
 #include "pch.h"
 
+static void    CheckData(GKEntity entity, int_t dataId1, void* data1, int_t dataId2, void* data2)
+{
+    void* data = GetExternalReferenceData(entity, dataId1);
+    ASSERT(data==data1);
+
+    data = GetExternalReferenceData(entity, dataId2);
+    ASSERT(data == data2);
+
+}
+
+static void    SetGetData(GKEntity entity, int_t dataId1, int_t dataId2)
+{
+    CheckData(entity, dataId1, 0, dataId2, 0);
+
+    auto res = SetExternalReferenceData(entity, dataId1, (void*)77);
+    ASSERT(res);
+    CheckData(entity, dataId1, (void*)77, dataId2, 0);
+
+    res = SetExternalReferenceData(entity, dataId2, (void*)9);
+    ASSERT(res);
+    CheckData(entity, dataId1, (void*)77, dataId2, (void*)9);
+
+    res = SetExternalReferenceData(entity, dataId1, 0);
+    ASSERT(res);
+    CheckData(entity, dataId1, 0, dataId2, (void*)9);
+
+}
+
 extern void ExtAppMemTest()
 {
     ENTER_TEST;
 
     OwlModel model = OpenModel(NULL);
     ASSERT(model);
-
+    
     auto mem = AllocModelMemory(model, 12);
     *((char*)mem) = 'A';
 
@@ -14,10 +42,22 @@ extern void ExtAppMemTest()
     ASSERT(dataId == 1);
     dataId = GetExternalReferenceDataId(model, "AnotherApp");
     ASSERT(dataId == 2);
-    dataId = GetExternalReferenceDataId(model, "app3");
-    ASSERT(dataId == 3);
+    auto dataId3 = GetExternalReferenceDataId(model, "app3");
+    ASSERT(dataId3 == 3);
     dataId = GetExternalReferenceDataId(model, "AnotherApp");
     ASSERT(dataId == 2);
+
+    SetGetData(model, dataId, dataId3);
+
+    auto prop = GetPropertyByName(model, "width");
+    ASSERT(prop);
+
+    SetGetData(prop, dataId3, dataId);
+
+    auto internalClass = GetClassByName(model, "Box");
+    ASSERT(internalClass);
+
+    SetGetData(internalClass, dataId, dataId3);
 
     CloseModel(model);
 
--- CppEngineTests/MetaProperties.cpp
+++ CppEngineTests/MetaProperties.cpp
@@ -28,11 +28,12 @@ static void SetGetObjecttypeProp(OwlClass cls, const char* propName, OwlInstance
 
     auto prop = GetPropertyByName(model, propName);
     ASSERT(prop);
+    ASSERT(model == GetModel(prop));
 
     int64_t card = 0;
     OwlInstance* val = NULL;
-    int64_t res = 0;// GetObjectTypeProperty(cls, prop, &val, &card);
-    //ASSERT(card == 0 && res == 0);
+    int64_t res = GetObjectTypeProperty(cls, prop, &val, &card);
+    ASSERT(card == 0 && res == 0);
 
     res = SetObjectTypeProperty(cls, prop, &value, 1);
     ASSERT(res == 0);
@@ -48,14 +49,17 @@ extern void MetaPropertiesTest()
 
     OwlModel model = OpenModel(NULL);
     ASSERT(model);
+    ASSERT(model == GetModel(model));
 
     CreateProperty(model, DATATYPEPROPERTY_TYPE_CHAR, "MyPropery");
     CreateProperty(model, OBJECTTYPEPROPERTY_TYPE, "MyObjectPropery");
 
     auto internalClass = GetClassByName(model, "Box");
     ASSERT(internalClass);
+    ASSERT(model == GetModel(internalClass));
 
     auto inst = GEOM::Material::Create(model);
+    ASSERT(model == GetModel(inst));
 
     SetGetDatatypeProp(internalClass, "length", 2.4);
     SetGetDatatypeProp(internalClass, "MyPropery", "my valye");
