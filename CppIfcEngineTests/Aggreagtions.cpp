325d114ee0dda92dc688b88cd01acf5a0da7e183
I.Sokolov <i.sokolov@ya.ru>
2024-08-07 09:40:45 +0300

    DeleteAggrByIndex

--- CppIfcEngineTests/Aggreagtions.cpp
+++ CppIfcEngineTests/Aggreagtions.cpp
@@ -60,6 +60,60 @@ static void DeleteWallsByIndex(bool subtypes)
     sdaiCloseModel(model);
 }
 
+static void CheckAggrElem(SdaiInstance inst, const char* attr, SdaiInteger index, int stepId)
+{
+    SdaiAggr aggr = 0;
+    auto ret = sdaiGetAttrBN(inst, attr, sdaiAGGR, &aggr);
+    ASSERT(ret == aggr && aggr);
+
+    auto cnt = sdaiGetMemberCount(aggr);
+    ASSERT(index < cnt);
+
+    SdaiInstance val = 0;
+    ret = sdaiGetAggrByIndex(aggr, index, sdaiINSTANCE, &val);
+    ASSERT(ret == (void*)val && val);
+
+    auto sid = internalGetP21Line(val);
+    ASSERT(sid == stepId);
+}
+
+
+static void DeleteAttrAggrByIndex()
+{
+    SdaiModel   model = sdaiOpenModelBN(0, "Walls.ifc", "");
+
+    auto unitAssmt = internalGetInstanceFromP21Line(model, 975);
+
+    CheckAggrElem (unitAssmt, "Units", 0, 947);
+
+    SdaiAggr units = NULL;
+    sdaiGetAttrBN(unitAssmt, "Units", sdaiAGGR, &units);
+    ASSERT(units);
+
+    xxxxDeleteFromAggregation(unitAssmt, units, 0);
+
+    CheckAggrElem(unitAssmt, "Units", 0, 948);
+ 
+    CheckAggrElem(unitAssmt, "Units", 2, 950);
+
+    xxxxDeleteFromAggregation(unitAssmt, units, 2);
+
+    CheckAggrElem(unitAssmt, "Units", 2, 951);
+
+    SdaiInteger last = sdaiGetMemberCount(units) - 1;
+
+    CheckAggrElem(unitAssmt, "Units", last, 858);
+
+    xxxxDeleteFromAggregation(unitAssmt, units, last);
+
+    ASSERT(sdaiGetMemberCount(units) == last);
+
+    CheckAggrElem(unitAssmt, "Units", last-1, 857);
+
+    //sdaiSaveModelBN(model, "Walls_del.ifc");
+    sdaiCloseModel(model);
+}
+
 
 extern void AggregationTests()
 {
@@ -70,5 +124,7 @@ extern void AggregationTests()
 
     DeleteWallsByIndex(true);
     DeleteWallsByIndex(false);
+
+    DeleteAttrAggrByIndex();
 
 }
