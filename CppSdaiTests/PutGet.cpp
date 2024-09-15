75742d84b486aa531a69d1ffa0958eb54ee703dd
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2024-09-15 12:18:47 +0400

    SDAI tests placeholder

---
+++ CppSdaiTests/PutGet.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "tchar.h"
+
+extern void GetTest()
+{
+    TCHAR t;
+}
---
+++ CppSdaiTests/main.cpp
@@ -0,0 +1,10 @@
+// CppSdaiTests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
