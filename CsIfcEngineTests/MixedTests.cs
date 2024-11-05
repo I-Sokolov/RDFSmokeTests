06a36e8852b749b512b491a1a822787df407bbff
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2024-11-05 15:44:35 +0400

    GetSetGlobalPlacement

---
+++ CsIfcEngineTests/MixedTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDF;
+
+namespace CsIfcEngineTests
+{
+    internal class MixedTests : CsTests.TestBase
+    {
+        public static void Run()
+        {
+            ENTER_TEST();
+            GetSetGlobalPlacement();
+        }
+
+        private static void GetSetGlobalPlacement()
+        {
+            var model = ifcengine.sdaiOpenModelBN(0, "..\\TestData\\walls.ifc", "");
+            ASSERT(model != 0);
+
+            //expected identity placement
+            double[] expectedOrigin = new double[12];
+            expectedOrigin[0] = expectedOrigin[4] = expectedOrigin[8] = 1.0;
+
+            double[] getOrigin = new double[12];
+            ifcengine.getGlobalPlacement(model, out getOrigin[0]);
+            ASSERT_EQ(getOrigin, expectedOrigin);
+
+            //now set translation (1,2,3)
+            expectedOrigin[9] = 1.0;
+            expectedOrigin[10] = 2.0;
+            expectedOrigin[11] = 3.0;
+            ifcengine.setGlobalPlacement(model, ref expectedOrigin[0], 0);
+
+            //check
+            ifcengine.getGlobalPlacement(model, out getOrigin[0]);
+            ASSERT_EQ(getOrigin, expectedOrigin);
+
+            ifcengine.sdaiCloseModel(model);
+        }
+
+    }
+}
--- CsIfcEngineTests/Program.cs
+++ CsIfcEngineTests/Program.cs
@@ -27,6 +27,7 @@ namespace CsIfcEngineTests
                 Console.WriteLine("--------- Starting IFC engine C# tests");
                 //RDF.ifcengine.setStringUnicode(1);`
 
+                MixedTests.Run();
                 RegionalChars.Run();
                 ExpressParser.Run();
                 PutGetAttr.Run();
