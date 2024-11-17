fddcaeede953c5cf64585d3b4ba205b88a1a6033
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2024-11-17 18:31:02 +0400

    save * test

--- CppIfcEngineTests/DerivedAttrs.cpp
+++ CppIfcEngineTests/DerivedAttrs.cpp
@@ -1,37 +1,57 @@
 #include "pch.h"
 
+#define TEST_FILE "DeriveAttr.ifc"
+
+static void DimensionalExponents_Check(SdaiModel model, bool calculated)
+{
+    IFC4::IfcSIUnit lengthUnit = internalGetInstanceFromP21Line(model, 391);
+    ASSERT(lengthUnit);
+
+    if (calculated) {
+        auto exponents = lengthUnit.get_Dimensions();
+        ASSERT(exponents);
+
+        auto exp = exponents.get_LengthExponent();
+        ASSERT(!exp.IsNull() && exp.Value() == 1);
+
+        exp = exponents.get_TimeExponent();
+        ASSERT(!exp.IsNull() && exp.Value() == 0);
+    }
+    else {
+        //
+        auto exponents = lengthUnit.get_Dimensions();
+        ASSERT(!exponents);
+    }
+}
+
+//int
+//ID 32
+//CoordinateSpaceDimension
+
 static void DimensionalExponents()
 {
     auto model = sdaiOpenModelBN(0, "..\\TestData\\AggregationTest.ifc", "");
     ASSERT(model);
 
-    IFC4::IfcSIUnit lengthUnit = internalGetInstanceFromP21Line(model, 391);
-    ASSERT(lengthUnit);
+    DimensionalExponents_Check(model, false);
 
-    //
-    auto exponents = lengthUnit.get_Dimensions();
-    ASSERT(!exponents);
-
-    //
     bool ok = engiSetDerivedAttributesSupport(model, true, false);
     ASSERT(ok);
 
-    exponents = lengthUnit.get_Dimensions();
-    ASSERT(exponents);
+    DimensionalExponents_Check(model, true);
 
-    auto exp = exponents.get_LengthExponent();
-    ASSERT(!exp.IsNull() && exp.Value() == 1);
-
-    exp = exponents.get_TimeExponent();
-    ASSERT(!exp.IsNull() && exp.Value() == 0);
-
-    //
     ok = engiSetDerivedAttributesSupport(model, false, true);
     ASSERT(ok);
 
-    exponents = lengthUnit.get_Dimensions();
-    ASSERT(!exponents);
+    DimensionalExponents_Check(model, false);
 
+    sdaiSaveModelBN(model, TEST_FILE);
+    sdaiCloseModel(model);
+
+    model = sdaiOpenModelBN(0, TEST_FILE, "");
+    ASSERT(model);
+
+    DimensionalExponents_Check(model, true);
 
     sdaiCloseModel(model);
 }
