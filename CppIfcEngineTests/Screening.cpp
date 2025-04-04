59d7ff862e9d38631f90bbad9660e5df20f3e497
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2025-04-04 12:54:23 +0400

    performance check

--- CppIfcEngineTests/Screening.cpp
+++ CppIfcEngineTests/Screening.cpp
@@ -1,9 +1,12 @@
 #include "pch.h"
 #include "engine.h"
 #include <Windows.h>
+#include <chrono>
 
 #define ENABLE_SMOKE_TESTS
 #include "SmokeTests.h"
+
+#if 0
 
 #define DATASET_IFC "C:\\DevArea\\RDF\\TestSets\\IfcEthalons\\"
 #define DATASET_AP242 "C:\\DevArea\\RDF\\TestSets\\STEP\\"
@@ -124,4 +127,65 @@ extern void Screening(void)
 
     PrintStatistic(statIfc, "IFC");
     PrintStatistic(statStep, "STEP");
+}
+
+#endif
+
+static void CheckPerformance(const char* path, double size)
+{
+    printf("File size %g GB\t", size);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    //
+    auto model = sdaiOpenModelBN(0, path, "");
+    
+    auto read = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> readTime = read - start;
+    printf("Read time %g sec\t", readTime.count());
+
+    //
+    auto product = sdaiGetEntity(model, "IfcProduct");
+    ASSERT(product);
+
+    auto isDefinedBy = sdaiGetAttrDefinition(product, "IsDefinedBy");
+    ASSERT(product);
+
+    int_t nProducts = 0;
+    int_t nPropset = 0;
+    auto aggr = xxxxGetEntityAndSubTypesExtent(model, product);
+    auto it = sdaiCreateIterator(aggr);
+    while (sdaiNext(it)) {
+        nProducts++;
+
+        SdaiInstance product = NULL;
+        sdaiGetAggrByIterator(it, sdaiINSTANCE, &product);
+
+        CalculateInstance(product);
+
+        SdaiAggr props = 0;
+        sdaiGetAttr(product, isDefinedBy, sdaiAGGR, &props);
+        nPropset += sdaiGetMemberCount(props);
+    }
+    sdaiDeleteIterator(it);
+
+    auto proceed = std::chrono::high_resolution_clock::now();
+
+    //
+    std::chrono::duration<double> proceedTime = proceed - read;
+
+    printf ("process time %g sec\t%lld products\t%lld propsets\n", proceedTime.count(), nProducts, nPropset);
+
+    sdaiCloseModel(model);
+
+}
+
+extern void Screening(void)
+{
+    CheckPerformance(R"(C:\Users\igor\OneDrive - Bentley Systems, Inc\IFC4RV_cert\IFC_files\Beam_01A_ABD.ifc)", 0);
+    CheckPerformance(R"(C:\Users\igor\OneDrive - Bentley Systems, Inc\IFC big files\BigIFC\Master-HiRiseScheme.ifc)", 0.15);
+    CheckPerformance(R"(C:\Users\igor\OneDrive - Bentley Systems, Inc\IFC big files\BigIFC\RailWaySupports.ifc)", 1.78);
+    CheckPerformance(R"(C:\Users\igor\OneDrive - Bentley Systems, Inc\IFC big files\05K-CAB01_2x3_20240305022956.ifc)", 2.13);
+    CheckPerformance(R"(C:\Users\igor\OneDrive - Bentley Systems, Inc\IFC big files\972305_Huge_IFC_ImportCrahs\0-general-ow-cj.ifc)", 3.87);
 }
